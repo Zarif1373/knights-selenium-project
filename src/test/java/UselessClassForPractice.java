33e8ae73f35781b899b4f984116b109a3cb00cac
Zarif1373 <146789780+Zarif1373@users.noreply.github.com>
2023-10-04 13:16:44 -0500

    Update UselessClassForPractice.java

--- src/test/java/UselessClassForPractice.java
+++ src/test/java/UselessClassForPractice.java
@@ -2,5 +2,6 @@ public class UselessClassForPractice {
 
     public static void main(String[] args) {
         System.out.println("UselessClassForPractice");
+        System.out.println("New changes");
     }
 }
